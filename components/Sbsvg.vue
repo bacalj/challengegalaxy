891d2541d49fcf1b104bdfb70572c14f76c47ab1
Joe Bacal <bacalj@gmail.com>
2019-02-18 19:13:31 -0500

    this way to do svgs

---
+++ components/Sbsvg.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <img :src="srcstring" alt="image of a scratch block">
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        sbid: String
+    },
+
+    computed:{
+        srcstring(){
+            return `/scratchblocksvgs/${this.sbid}.svg`
+        }
+    }
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,58 +1,21 @@
 <template>
-  <section class="container">
-    <div>
-      <img src="~/assets/scratchblocksvgs/when_green_flag_clicked.svg" alt="">
-      <logo />
-      <h1 class="title">
-        challengegalaxy
-      </h1>
-      <h2 class="subtitle">
-        programming challenges for everyone
-      </h2>
-      </div>
-    </div>
-  </section>
+    <section class="container">
+        <div>
+            <h1 class="title">challengegalaxy</h1>
+            <sbsvg sbid="when_green_flag_clicked" />
+        </div>
+    </section>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
+import Sbsvg from '~/components/Sbsvg.vue'
 
 export default {
   components: {
-    Logo
+    Sbsvg
   }
 }
 </script>
 
 <style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
 </style>
