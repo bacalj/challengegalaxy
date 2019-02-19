8dc833bf4e60591f23a5d75ac8c2f43896f03afa
Joe Bacal <bacalj@gmail.com>
2019-02-18 22:21:08 -0500

    an index page for no challenge specified

---
+++ pages/challenges/index.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        hey why r u here - choose a challenge
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style scoped>
+
+</style>
