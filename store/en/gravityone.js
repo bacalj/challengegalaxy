552498bd0fafe40e5ccf26ffb843736cb28a63fb
Joe Bacal <bacalj@gmail.com>
2019-02-18 22:20:52 -0500

    data

--- store/en/gravityone.js
+++ store/en/gravityone.js
@@ -1,11 +1,16 @@
 export const state = () => ({
     title: 'Gravity One',
+    goalImg: 'goal img',
+    goalText: 'goal text',
+
     clues:[
         {
+            id: 1,
             img: 'i am image',
             text: 'i am text'
         },
         {
+            id: 2,
             img: 'i am another image',
             text: 'i am another text'
         }
--- store/sp/gravityone.js
+++ store/sp/gravityone.js
@@ -1,11 +1,16 @@
 export const state = () => ({
     title: 'Gravidad Uno',
+    goalImg: 'goal img',
+    goalText: 'goal texto',
+
     clues:[
         {
+            id: 1,
             img: 'estoy imagen',
             text: 'estoy texto'
         },
         {
+            id: 2,
             img: 'estoy otro imagen',
             text: 'estoy otro texto'
         }
