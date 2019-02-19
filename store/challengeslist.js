62a6e641e9eab11f4cce5c53f62479c5f58ea920
Joe Bacal <bacalj@gmail.com>
2019-02-19 18:35:22 -0500

    putting a master challenges list in state

---
+++ store/challengeslist.js
@@ -0,0 +1,9 @@
+export const state = () => ({
+    published:[
+        { 
+            id: 'hat-landing', 
+            link: 'challenges/hat-landing', 
+            title: 'Hat Landing'
+        }
+    ]
+})
