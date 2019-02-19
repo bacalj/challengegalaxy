0d5e27eab2f12b441ab007b4ffa3b6de9a700bfd
Joe Bacal <bacalj@gmail.com>
2019-02-18 22:22:06 -0500

    clue component

---
+++ components/AClue.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        {{ img }}
+        {{ text }}
+    </div>
+</template>
+
+<script>
+export default {
+    props:[
+        'text',
+        'img'
+    ]
+}
+</script>
+
+<style scoped>
+
+</style>
