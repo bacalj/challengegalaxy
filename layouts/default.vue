bc6f7560b27ac61314b44e43b554d84a39ee2741
Joe Bacal <bacalj@gmail.com>
2019-02-19 18:22:47 -0500

    cleanup default layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,19 @@
 <template>
   <div>
+    <TheNavbar />
     <nuxt />
   </div>
 </template>
+
+<script>
+import TheNavbar from '~/components/TheNavbar.vue'
+export default {
+     components:{
+        TheNavbar
+    } 
+}
+</script>
+
 
 <style>
 html {
