698698ae1029a6791e0fc0fa650768bab6f2b56c
Joe Bacal <bacalj@gmail.com>
2019-02-19 18:35:46 -0500

    hat landing single challenge data file

---
+++ store/en/hat-landing.js
@@ -0,0 +1,26 @@
+export const state = () => ({
+    id: 'hat-landing',
+    title: 'Hat Landing',
+    goalImg: '/gifs/hat_landing.gif',
+    goalText: 'Make something land on something',
+    scratchprojid: 287880895,
+
+    clues:[
+        {
+            id: 1,
+            content: '/pngs/when_green_flag_clicked.png',
+        },
+        {
+            id: 2,
+            content: '/pngs/change_y_by_neg_10.png',
+        },
+        {
+            id: 3, 
+            content: '/pngs/repeat_until.png'
+        },
+        {
+            id: 4, 
+            content: '/pngs/touching.png'
+        }
+    ]
+})
