88bb3abea9af4167854fc20f168448f66a8e190d
Joe Bacal <bacalj@gmail.com>
2019-02-19 19:04:51 -0500

    attempt and fail to get bulma vars to be overrridable

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,72 +2,80 @@ const pkg = require('./package')
 
 
 module.exports = {
-  mode: 'universal',
+    mode: 'universal',
 
-  generate: {
-      routes: [
-          '/challenges/hat-landing'
-      ]
-  },
-  /*
-  ** Headers of the page
-  */
-  head: {
-    title: pkg.name,
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: pkg.description }
-    ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
-  },
-
-  /*
-  ** Customize the progress-bar color
-  */
-  loading: { color: '#fff' },
-
-  /*
-  ** Global CSS
-  */
-  css: [
-  ],
-
-  /*
-  ** Plugins to load before mounting the App
-  */
-  plugins: [
-  ],
-
-  /*
-  ** Nuxt.js modules
-  */
-  modules: [,
-    // Doc:https://github.com/nuxt-community/modules/tree/master/packages/bulma
-    '@nuxtjs/bulma'
-  ],
-
-  /*
-  ** Build configuration
-  */
-  build: {
-    extractCSS: {
-        allChunks: true
-    },
-    postcss: {
-      preset: {
-        features: {
-          customProperties: false
-        }
-      }
+    generate: {
+        routes: [
+            '/challenges/hat-landing'
+        ]
     },
     /*
-    ** You can extend webpack config here
+    ** Headers of the page
     */
-    extend(config, ctx) {
-      
+    head: {
+        title: pkg.name,
+        meta: [
+            { charset: 'utf-8' },
+            { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+            { hid: 'description', name: 'description', content: pkg.description }
+        ],
+        link: [
+            { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+        ]
+    },
+
+    /*
+    ** Customize the progress-bar color
+    */
+    loading: { color: '#fff' },
+
+    /*
+    ** Global CSS
+    */
+    css: [
+    ],
+
+    /*
+    ** Plugins to load before mounting the App
+    */
+    plugins: [
+    ],
+
+    /*
+    ** Nuxt.js modules
+    */
+    modules: [,
+        // Doc:https://github.com/nuxt-community/modules/tree/master/packages/bulma
+        '@nuxtjs/style-resources',
+        '@nuxtjs/bulma'
+    ],
+
+    styleResources:{
+        scss: [
+            './assets/vars/*.scss',
+            './assets/abstracts/_mixins.scss'
+        ]
+    },
+
+    /*
+    ** Build configuration
+    */
+    build: {
+        extractCSS: {
+            allChunks: true
+        },
+        postcss: {
+            preset: {
+                features: {
+                    customProperties: false
+                }
+            }
+        },
+        /*
+        ** You can extend webpack config here
+        */
+        extend(config, ctx) {
+
+        }
     }
-  }
 }
