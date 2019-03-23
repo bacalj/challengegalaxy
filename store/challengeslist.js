c2965bd8d01ca415372c181c81595e1fd63a156e
Joe Bacal <bacalj@gmail.com>
2019-03-23 18:00:31 -0400

    faking existence of a lot of challenges for styling's sake

--- store/challengeslist.js
+++ store/challengeslist.js
@@ -14,6 +14,81 @@ export const state = () => ({
             id: 'hat-landing', 
             link: 'challenges/hat-landing', 
             title: 'Hat Landing'
+        },
+        { 
+            id: 'maze', 
+            link: 'challenges/maze', 
+            title: 'Maze'
+        },
+        { 
+            id: 'video-sensing', 
+            link: 'challenges/video-sensing', 
+            title: 'Video Sensing'
+        },
+        { 
+            id: 'hat-landing', 
+            link: 'challenges/hat-landing', 
+            title: 'Hat Landing'
+        },
+        { 
+            id: 'maze', 
+            link: 'challenges/maze', 
+            title: 'Maze'
+        },
+        { 
+            id: 'video-sensing', 
+            link: 'challenges/video-sensing', 
+            title: 'Video Sensing'
+        },
+        { 
+            id: 'hat-landing', 
+            link: 'challenges/hat-landing', 
+            title: 'Hat Landing'
+        },
+        { 
+            id: 'maze', 
+            link: 'challenges/maze', 
+            title: 'Maze'
+        },
+        { 
+            id: 'video-sensing', 
+            link: 'challenges/video-sensing', 
+            title: 'Video Sensing'
+        },
+        { 
+            id: 'hat-landing', 
+            link: 'challenges/hat-landing', 
+            title: 'Hat Landing'
+        },
+        { 
+            id: 'maze', 
+            link: 'challenges/maze', 
+            title: 'Maze'
+        },
+        { 
+            id: 'video-sensing', 
+            link: 'challenges/video-sensing', 
+            title: 'Video Sensing'
+        },
+        { 
+            id: 'hat-landing', 
+            link: 'challenges/hat-landing', 
+            title: 'Hat Landing'
+        },
+        { 
+            id: 'maze', 
+            link: 'challenges/maze', 
+            title: 'Maze'
+        },
+        { 
+            id: 'video-sensing', 
+            link: 'challenges/video-sensing', 
+            title: 'Video Sensing'
+        },
+        { 
+            id: 'hat-landing', 
+            link: 'challenges/hat-landing', 
+            title: 'Hat Landing'
         }
     ],
 
