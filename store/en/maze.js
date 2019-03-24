227efec255355628f71fd6b6ac726975862ae83e
Joe Bacal <bacalj@gmail.com>
2019-03-24 17:18:40 -0400

    new favicon, staring to fix maze

--- store/en/maze.js
+++ store/en/maze.js
@@ -11,38 +11,43 @@ export const state = () => ({
             id: 1,
             stepNum: '1',
             stepImg: '/gifs/mouse_move_1.gif',
-            stepLabel: '<i class="fas fa-mouse-pointer"></i> <i class="fas fa-arrows-alt"></i>',
-            stepText: 'Move around!',
+            stepLabel: '<i class="fas fa-lightbulb"></i>',
+            stepText: 'Move with the mouse',
             clues:[
                 {
-                    cover: '<i class="fas fa-clock"></i>',
+                    cover: 'When?',
                     id: 1,
-                    content: '<pre class="blocks">when green flag clicked</pre>'
+                    content: '<pre class="blocks">when green flag clicked\nforever</pre>',
+                    type: 'code',
+                    showAtLevelAndAbove:"1"
                 },
                 {
-                    cover: '<i class="fas fa-clock"></i>',
                     id: 2,
-                    content: '<pre class="blocks">forever</pre>'
-                },
-                {
-                    id: 3,
                     cover: '<i class="fas fa-compass"></i>',
                     content: '<pre class="blocks">point towards ()</pre>',
+                    type: 'code',
+                    showAtLevelAndAbove:"1"
                 },
                 {
                     cover: '<i class="fas fa-compass"></i>',
                     id: 4, 
-                    content: '<pre class="blocks">point towards ( what should you point towards?! )</pre>'
+                    content: '<pre class="blocks">point towards ( what should you point towards?! )</pre>',
+                    type: 'idea',
+                    showAtLevelAndAbove:"1"
                 },
                 {
                     cover: '<i class="fas fa-arrows-alt"></i>',
                     id: 5,
-                    content: 'You point to where you want to go and then you...'
+                    content: 'You point to where you want to go and then you...',
+                    type: 'idea',
+                    showAtLevelAndAbove:"1"
                 },
                 {
                     cover: '<i class="fas fa-arrows-alt"></i>',
                     id: 6,
-                    content: '<pre class="blocks">move ( ) steps</pre>'
+                    content: '<pre class="blocks">move ( ) steps</pre>',
+                    type: 'code',
+                    showAtLevelAndAbove:"1"
                 }
             ]
         },
@@ -51,8 +56,8 @@ export const state = () => ({
             id: 2,
             stepNum: '2',
             stepImg: '/gifs/maze_bump_1.gif',
-            stepLabel: '<i class="fas fa-walking"></i> <i class="fas fa-ban"></i>',
-            stepText: 'Bump!',
+            stepLabel: '<i class="fas fa-lightbulb"></i>',
+            stepText: 'Stop at the walls',
             clues:[
                 {
                     cover: '<i class="fas fa-clock"></i>',
@@ -80,7 +85,7 @@ export const state = () => ({
             id: 3,
             stepNum: '3',
             stepImg: '/gifs/maze_glitch.gif',
-            stepLabel: '<i class="fas fa-bug"></i><i class="fas fa-ban"></i>',
+            stepLabel: '<i class="fas fa-lightbulb"></i>',
             stepText: 'Glitch!',
             clues:[
                 {
@@ -104,7 +109,7 @@ export const state = () => ({
             id: 4,
             stepNum: '4',
             stepImg: '/gifs/maze_win.gif',
-            stepLabel: '<i class="fas fa-award"></i>',
+            stepLabel: '<i class="fas fa-lightbulb"></i>',
             stepText: 'You win!',
             clues:[
                 {
