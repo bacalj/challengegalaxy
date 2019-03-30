3ed6684564b6085dc2b69695cf2b6d784f818484
Joe Bacal <bacalj@gmail.com>
2019-03-30 16:12:02 -0400

    initial langauge for about page

--- pages/about.vue
+++ pages/about.vue
@@ -1,6 +1,25 @@
 <template>
-    <section class="container">
-        This is the about page
+    <section class="container about-page-container">
+        <div class="columns">
+            <div class="column is-three-fifths is-offset-one-fifth has-text-light">
+                
+                <h1 class="title has-text-light">About ChallengeGalaxy</h1>
+                
+                <h2 class="subtitle has-text-light">Experimentation and Play</h2>
+                <p>Fiddling, puzzling, playing and experimenting are at the core of learning. The progressive educational movements of the last hundred years go by different monikers – constructivism, constructionism, “Maker Education,”“Hands on Learning.” The big idea that connects them is that the best learning happens when people engage with materials, deriving their own understanding through active inquiry and experimentation – rather than through instruction about the materials.  What are the core concepts of engineering? Try building a bridge out of drinking straws and marshmallows and you’ll probably arrive at a good portion of the answer.</p>
+                <br>
+
+                <h2 class="subtitle has-text-light">The Right Amount of Scaffolding</h2>
+                <p>The big challenge for educators is how to manage what should be a constant tension between the instinct to show someone how to do something and the harder task of laying out the pieces and engaging them in the task of figuring it out. Sometimes a student is stuck and could benefit from a bit of direction or information to keep moving forward. The challenge for educators to devise assignments for students that allows them to develop their own questions and methods, but still help them arrive at the particular understandings that we as the educators have been tasked with “teaching.”  At the end of the day we may really need to know how to build a real bridge.</p>
+                <br>
+                
+                <h2 class="subtitle has-text-light">A flexible platform</h2>
+                <p>The goal of ChallengeGalaxy is to create a platform that allows for this flexibility.  We are working to give learners a place where they can puzzle through the key concepts of programming with optional clues and pointers which can give a hand up over a wall, solidify knowledge, confirm suspicions, or open paths to new inquiries.  Some people will want a lot of pointers, some will want a few, and some will want none.</p>
+
+
+
+            </div>
+        </div>
     </section>
 </template>
 
@@ -9,5 +28,15 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+
+h1.title {
+    margin-top:60px;
+    margin-bottom:40px !important;
+}
+
+.column {
+    padding-left:20px;
+    padding-right:20px;
+}
 </style>
