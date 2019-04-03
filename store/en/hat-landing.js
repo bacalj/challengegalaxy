77c356ae29b866d17cfdcc1e35201d688c5a334c
Joe Bacal <bacalj@gmail.com>
2019-04-03 14:41:22 -0400

    add initial clues visible attribute to hat landing

--- store/en/hat-landing.js
+++ store/en/hat-landing.js
@@ -12,20 +12,19 @@ export const state = () => ({
             stepImg: '/gifs/hat_landing.gif',
             stepLabel: '<i class="fas fa-lightbulb"></i>',
             stepText: 'This is step 1.',
+            initialCluesVisible:0,
             clues:[
                 {
                     cover: 'Before you start',
                     id: 1, 
                     content: 'What will your sprites be?',
                     type: 'info',
-                    showAtLevelAndAbove:"1"
                 },
                 {
                     cover: 'Start here',
                     id: 2, 
                     content: '<pre class="blocks">when green flag clicked</pre>',
                     type: 'idea',
-                    showAtLevelAndAbove:"1"
                 },
 
                 {
@@ -33,21 +32,18 @@ export const state = () => ({
                     id: 3, 
                     content: '<pre class="blocks">forever</pre>',
                     type: 'code',
-                    showAtLevelAndAbove:"2"
                 },
                 {
                     cover: 'another scratchblock',
                     id: 4,
                     content: '<pre class="blocks">when green flag clicked\nforever\nturn cw (15) degrees\nsay [Hello!] for (2) secs\nif &lt;mouse down?&gt; then\nchange [mouse clicks v] by (1)\nend\n</pre>',
                     type: 'locked',
-                    showAtLevelAndAbove:"3"
                 },
                 {
                     cover: 'Bonus',
                     id: 5,
                     content: '<pre class="blocks">say [You are amazing!]</pre>',
                     type: 'extra',
-                    showAtLevelAndAbove:"0"
                 }
             ]
         }
