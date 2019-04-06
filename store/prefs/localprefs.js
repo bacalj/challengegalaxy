462014f8ffc2d3269a74d3704aa833e5b87a1e83
Joe Bacal <bacalj@gmail.com>
2019-04-06 10:29:56 -0400

    removing LevelSlider data and mutations from store

--- store/prefs/localprefs.js
+++ store/prefs/localprefs.js
@@ -1,11 +1,3 @@
 export const state = () => ({
-    //eventually get this from auth and db
-    levelSetTo: 1,
     fallingStars: false
 })
-
-export const mutations = {
-    setLevel(state, sliderVal){
-        state.levelSetTo = sliderVal;
-    }
-} 
