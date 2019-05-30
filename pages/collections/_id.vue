dc61e154f4276b3cdb73206b25f4b18c0cf2e978
Joe Bacal <bacalj@gmail.com>
2019-05-29 22:54:57 -0400

    stub out collection components

---
+++ pages/collections/_id.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        this is a single collection
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ pages/collections/index.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        this is the index of all the collections
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
