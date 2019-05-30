358a31f015d7908f7b9da7dcc7afaea0760b0e32
Joe Bacal <bacalj@gmail.com>
2019-05-29 22:51:33 -0400

    starting on collection w a note to self

---
+++ components/ACollection.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        this should be a place we get routed to - and we should ask the store for the challenges we need and loop the squares out just like we would on the front page or elsewhere
+
+        then, on the collections home page as it were, we would loop over the existing collections and for each of them output a new sort of card stack looking thing
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
