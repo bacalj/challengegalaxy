894ec42010f19e907926ca4873a0230d9654f728
Joe Bacal <bacalj@gmail.com>
2019-06-02 21:18:39 -0400

    stub content for collections index

--- pages/collections/index.vue
+++ pages/collections/index.vue
@@ -1,6 +1,14 @@
 <template>
     <div>
-        this is the index of all the collections
+        <section class="featured section has-background-dark has-text-centered">
+            <a name="featured" id="featured"></a>
+            <h2 class="title is-2">Find a Collection</h2>
+            <div class="columns">
+                <div class="column is-three-fifths is-offset-one-fifth has-text-light">
+                    Collections are groups of challenges that go together. 
+                </div>
+            </div>
+        </section>
     </div>
 </template>
 
