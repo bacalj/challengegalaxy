2381415a3e78a699b6ea73c39eaf63e48e68a7f3
Andrew Pasquale <andrew@elytra.net>
2019-06-09 13:09:21 -0400

    Use grunt-zip to package chrome extension.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,3 +1,5 @@
+var path = require('path');
+
 module.exports = function(grunt) {
     grunt.initConfig({
     // Move CSS and JS to _nuxt folder
@@ -5,14 +7,23 @@ module.exports = function(grunt) {
     // so the Chrome Extension can load the app from website.
     move: {
         move_css: {
-        src: 'dist/*.css',
-        dest: 'dist/_nuxt/'
+            src: 'dist/*.css',
+            dest: 'dist/_nuxt/'
         },
         move_js: {
             src: 'dist/*.js',
             dest: 'dist/_nuxt/'
-            }
-    }
+        }
+    },
+    // Zip the Chrome Extension for upload to the store.
+    zip: {
+        'using-cwd': {
+            cwd: 'dist/chrome-extension/',
+            src: ['dist/chrome-extension/*'],
+            dest: 'dist/chrome_extension.zip'
+        }
+      }
     });
     grunt.loadNpmTasks('grunt-move');
+    grunt.loadNpmTasks('grunt-zip');
 };
