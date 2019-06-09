8ca420d597db30f5b7005c6624a22aa3b6bf6be1
Andrew Pasquale <andrew@elytra.net>
2019-06-09 10:44:59 -0400

    tempoarily use subtest url for challenge links.

--- static/chrome-extension/contentScript.js
+++ static/chrome-extension/contentScript.js
@@ -1,6 +1,6 @@
 const loadTutorial = function loadTutorial(id, title) {
 
-    const html = "<div class='tutorial draggable'><div class='tutorial-header'><div class='tutorial-title'><img src='" + chrome.extension.getURL('/images/spacescratch-128.png') + "' style='width: 32px'> <span>" + title + "</span></div><div style='display: flex'><div class='card_shrink-expand-button_vbegz'><img src='/static/assets/76121b7f4eff7d7ca0d49a45479d3f3f.svg'><span>Shrink</span></div><div class='close-tutorial card_remove-button_1F8SI'><img class='card_close-icon_1FYf5' src='/static/assets/a3e689235188ba10c5cfd75730cb88a7.svg'><span>Close</span></div></div></div><div class='tutorial-content'><iframe src='https://www.challengegalaxy.com/extension/"+id+"'/></div></div>";
+    const html = "<div class='tutorial draggable'><div class='tutorial-header'><div class='tutorial-title'><img src='" + chrome.extension.getURL('/images/spacescratch-128.png') + "' style='width: 32px'> <span>" + title + "</span></div><div style='display: flex'><div class='card_shrink-expand-button_vbegz'><img src='/static/assets/76121b7f4eff7d7ca0d49a45479d3f3f.svg'><span>Shrink</span></div><div class='close-tutorial card_remove-button_1F8SI'><img class='card_close-icon_1FYf5' src='/static/assets/a3e689235188ba10c5cfd75730cb88a7.svg'><span>Close</span></div></div></div><div class='tutorial-content'><iframe src='https://subtest.challengegalaxy.com/extension/"+id+"'/></div></div>";
 
     document.querySelector(".gui").insertAdjacentHTML("afterbegin", html );
     $(".draggable" ).draggable();
