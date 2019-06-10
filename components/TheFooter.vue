fcb696d62ec45ce1d7f5e39924b34ac7b243f7a7
Andrew Pasquale <andrew@elytra.net>
2019-06-10 11:41:44 -0400

    Add contact link to footer

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -2,10 +2,9 @@
     <div>
         <footer class="footer has-background-dark has-text-light">
             <div class="content has-text-centered">
-                <p>ChallengeGalaxy</p>
+                <p>ChallengeGalaxy | <nuxt-link to="/contact">Contact Us</nuxt-link></p>
                 <p>The source code is licensed
-                    <a href="http://opensource.org/licenses/mit-license.php">MIT</a>. The website content
-                    is licensed <a href="http://creativecommons.org/licenses/by-nc-sa/4.0/">CC BY NC SA 4.0</a>.
+                    <a href="http://opensource.org/licenses/mit-license.php">MIT</a>. The website content is licensed <a href="http://creativecommons.org/licenses/by-nc-sa/4.0/">CC BY NC SA 4.0</a>.
                 </p>
             </div>
         </footer>
@@ -18,6 +17,5 @@
     }
 </script>
 
-<style scoped>
-
+<style lang="scss" scoped>
 </style>
