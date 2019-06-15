e658c803516a7710eb3db8a17ab26cec30c6a300
Joe Bacal <bacalj@gmail.com>
2019-06-15 08:49:45 -0400

    learn-name challenge content

--- store/en/learn-name.js
+++ store/en/learn-name.js
@@ -1,90 +1,74 @@
 export const state = () => ({
     id: 'learn-name',
-    title: 'Teach A Cat Your Name',
+    title: 'Teach An Elephant!',
     goalImg: '/gifs/name.gif',
-    goalText: 'Make it land on something',
+    goalText: 'Save and use something typed in',
     scratchprojid: 316775974,
+    isUnderConstruction:true,
 
     steps: [
         {
             id: 1,
             stepNum: '1',
-            stepImg: '/hat-landing/hat_landing_setup.jpg',
-            stepLabel: '<i class="fas fa-lightbulb"></i> Get Set Up!',
-            stepText: 'Choose two sprites',
+            stepImg: '/learn-name/asking_name.jpg',
+            stepLabel: '<i class="far fa-comment"></i></i> Make it ask!',
+            stepText: 'Choose a sprite and make it ask a question',
             initialCluesVisible:0,
             clues:[
                 {
-                    cover: 'Use your imagination!',
+                    cover: 'When the person clicks the green flag...',
                     id: 1, 
-                    content: "<img src='/svgs/unicorn.svg' alt='unicorn'/><p>You can choose any sprites and backdrops you like for this project. In some places you may need to adapt the clues if you choose something different. Remember this challenge is meant to inspire your own ideas!</p>",
-                    type: 'info',
+                    content: '<pre class="blocks">when green flag clicked</pre>',
+                    type: 'code',
+                    sprite: 'elephant.png'
                 },
                 {
-                    cover: 'Start here to choose a sprite',
+                    cover: 'Ask them something!"',
                     id: 2, 
-                    content: '/gifs/choose-a-sprite.gif',
-                    type: 'info',
-                },
-                {
-                    cover: 'Start here to choose a backdrop',
-                    id: 3, 
-                    content: '/gifs/choose-a-backdrop.gif',
-                    type: 'info',
+                    content: '<pre class="blocks">ask [ What\'s your name? ] and wait</pre>',
+                    type: 'code',
+                    sprite: 'elephant.png'
                 }
             ]
         },
         {
             id: 2,
             stepNum: '2',
-            stepImg: '/gifs/hat_landing.gif',
-            stepLabel: '<i class="fas fa-lightbulb"></i> Make it work!',
-            stepText: 'Make one sprite fall until it lands on the other',
+            stepImg: '/learn-name/saying_name.jpg',
+            stepLabel: '<i class="fas fa-comment"></i></i> Make it answer!',
+            stepText: 'Make the sprite repeat the person\'s answer',
             initialCluesVisible:0,
             clues:[
                 {
-                    cover: 'How will it always start on top?',
+                    cover: 'The elephant keeps the answer in a variable called...',
                     id: 1, 
-                    content: '<pre class="blocks">when flag clicked</pre>',
+                    content: '<pre class="blocks">answer</pre>',
                     type: 'code',
-                    sprite: 'hat.png'
+                    sprite: 'elephant.png'
                 },
                 {
-                    cover: 'How will it always start on top?',
+                    cover: 'The join block puts words together',
                     id: 2, 
-                    content: '<pre class="blocks">go to x:() y:()</pre><img src="/pngs/xygrid.png">',
+                    content: '<pre class="blocks">join()()</pre>',
                     type: 'code',
-                    sprite: 'hat.png'
+                    sprite: 'elephant.png'
                 },
                 {
-                    cover: 'How will it fall on its own?',
+                    cover: 'Blocks can fit in other blocks...',
                     id: 3,
-                    content:'<pre class="blocks">change y by:()</pre>',
+                    content: '<pre class="blocks">say() \n join[Hello, ]() \n answer</pre>',
                     type: 'code',
-                    sprite: 'hat.png'
+                    sprite: 'elephant.png'
                 },
                 {
-                    cover: 'How will it keep falling (until it lands)?',
-                    id: 4, 
-                    content: '<pre class="blocks">repeat until &lt; &gt; \n \n &lt; touching (sprite1 v)?&gt;</pre>',
-                    type: 'code',
-                    sprite: 'hat.png'
-                },
-                {
-                    cover: 'Make it move a little futher.',
-                    id: 5, 
-                    content: '<pre class="blocks">change y by (-20)</pre>',
-                    type: 'code',
-                    sprite: 'hat.png'
-                },
-                {
-                    cover: 'Keep experimenting before you click this open!',
-                    id: 6,
-                    content: '<pre class="blocks">when green flag clicked\n go to x:(0) y:(165)\n repeat until &lt; touching (sprite2 v) ? &gt;\n &emsp; change y by (-10)\n end\n change y by (-20)</pre>',
+                    cover: 'Fit them all together',
+                    id: 4,
+                    content:'<pre class="blocks">When green flag clicked \n ask [ What\'s your name? ] and wait \n say (join[hello](answer))</pre>',
                     type: 'locked',
-                    sprite: 'hat.png'
+                    sprite: 'elephant.png'
                 }
             ]
-        }
+        },
+        
     ],
 })
