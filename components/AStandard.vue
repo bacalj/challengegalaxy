e7b43f4a9be3e47df38466549f4ec11c3e0d2aa9
Andrew Pasquale <andrew@elytra.net>
2019-06-19 23:07:28 -0400

    Standards alignment matrix

---
+++ components/AStandard.vue
@@ -0,0 +1,44 @@
+<template>
+        <tr>
+            <th><nuxt-link :to=link>{{ title }}</nuxt-link></th>
+            <td v-for="(s, index) in this.standards[standard]" v-bind:key="s.key">
+                <i v-if="matchStandard(index)" class="fas fa-check"></i>
+            </td>
+        </tr>
+</template>
+
+<script>
+export default {
+    data(){
+        return { 
+            link: "/challenges/" + this.id,
+        }
+    },
+    methods: {
+        matchStandard(index) {
+            if (this.standards[this.standard] && this.challengeStandards[this.standard]) {
+                const standard = this.standards[this.standard][index].id
+                return this.challengeStandards[this.standard].indexOf(standard) != -1
+            }
+        }
+    },
+    computed: {
+        standards(){
+            return this.$store.state.standards
+        },
+    },  
+    props:[
+        'title',
+        'standard',
+        'challengeStandards',
+        'id'
+    ]
+}
+</script>
+
+<style <style lang="scss" scoped>
+    td {
+        text-align: center;
+        vertical-align: middle;
+    }
+</style>
---
+++ components/AStandardHeader.vue
@@ -0,0 +1,27 @@
+<template>
+    <th>
+        <a :href=this.link>
+            <abbr :title=description>
+                {{ abbr }}
+            </abbr>
+        </a>
+    </th>
+</template>
+
+<script>
+export default {
+    props:[
+        'description',
+        'link',
+        'abbr',
+    ],
+}
+
+</script>
+
+<style lang="scss" scoped>
+    th {
+        font-size: 0.8rem;
+        min-width: 4.6rem;
+    }
+</style>
