a2f790cebed60a99489205f45c43bfa0da39149f
Joe Bacal <bacalj@gmail.com>
2019-06-21 19:58:38 -0400

    this gets collections in the drop down but data still not reactive so going to hard code for now

--- pages/challenges/index.vue
+++ pages/challenges/index.vue
@@ -3,7 +3,24 @@
         <section class="featured section has-background-dark has-text-centered">
             <a name="featured" id="featured"></a>
             <h1 class="title is-1">Find a Challenge</h1>
-            <!-- <button @click="sortByTitle">Sort by title test</button> -->
+            <div class="selects">
+                <div class="select">
+                    <select v-model="levelFilterVal">
+                        <option value="All Levels">All Levels</option>
+                        <option>Just Starting</option>
+                        <option>Some Experience</option>
+                        <option>Lots of Experience</option>
+                    </select>
+                </div>
+            
+                <div class="select">
+                    <select v-model="collectionFilterVal">
+                        <option value="All Collections">All Collections</option>
+                        <option :value="collection.title" :key="collection.id" v-for="collection in collectionOptions">{{ collection.title }}</option>
+                    </select>
+                </div>
+            </div>
+            <button @click="applyTheFilters">apply them filters</button>
             <div class="columns is-multiline">
                 <a-challenge
                     v-for="challenge in challenges"
@@ -11,6 +28,7 @@
                     :key="challenge.id">
                 </a-challenge>
             </div>
+
         </section>
     </div>
 </template>
@@ -26,15 +44,28 @@ export default {
     
     transition: 'pagechange',
     
+    data(){
+        return {
+            levelFilterVal: 'All Levels',
+            collectionFilterVal: 'All Collections',
+            collectionOptions: this.$store.state.collections.collectionslist.published
+        }
+    },
+
     computed: {
         ...mapState({
             challenges: state => state.challengeslist.published,
+            collections: state => state.collections.collectionslist.published
         }),
     },
 
     methods: {
         sortByTitle(){
             this.$store.commit('sortChallengesByTitle');
+        },
+
+        applyTheFilters(){
+            this.$store.commit('applyFilters', this.levelFilterVal, this.collectionFilterVal);
         }
     },
 
--- store/index.js
+++ store/index.js
@@ -9,5 +9,12 @@ export const getters = {
 export const mutations = {
     sortChallengesByTitle(state){
         state.challengeslist.published = state.challengeslist.published.sort((a, b) => (a.title > b.title) ? 1 : -1);
+    },
+
+    applyFilters(state, levelFilter, collectFilter){
+        console.log(levelFilter);
+        console.log(collectFilter);
+        console.log(state.challengeslist.published);
+        console.log(state.collections.collectionslist);
     }
 }
