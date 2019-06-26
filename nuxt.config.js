15fe15ef317ccc27704cfc9417c8c7729ff24eaf
Andrew Pasquale <andrew@elytra.net>
2019-06-26 15:43:39 -0400

    Update font awesome and import locally

--- nuxt.config.js
+++ nuxt.config.js
@@ -77,14 +77,6 @@ module.exports = {
         ],
         link: [
             { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-        ],
-        link: [
-            { 
-                rel: 'stylesheet', 
-                href:'https://use.fontawesome.com/releases/v5.7.2/css/all.css',  
-                integrity: 'sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr', 
-                crossorigin:'anonymous' 
-            }
         ]
     },
 
