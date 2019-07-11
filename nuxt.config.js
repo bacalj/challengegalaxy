88266cbcd7555201e0a319ad34f4b92647c9ce78
Joe Bacal <bacalj@gmail.com>
2019-07-11 10:20:17 -0400

    meta tag for fb key image

--- nuxt.config.js
+++ nuxt.config.js
@@ -75,7 +75,10 @@ module.exports = {
         meta: [
             { charset: 'utf-8' },
             { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-            { hid: 'description', name: 'description', content: pkg.description }
+            { hid: 'description', name: 'description', content: pkg.description },
+            { property: 'og:image', content: '/pngs/cgfb.png'}
+
+          
         ],
         link: [
             { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
