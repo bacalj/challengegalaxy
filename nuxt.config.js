e9894c360e17acad0538a8cad18e4bc0d315522e
Joe Bacal <bacalj@gmail.com>
2019-07-11 10:50:11 -0400

    changing description that propagates to meta

--- nuxt.config.js
+++ nuxt.config.js
@@ -76,9 +76,7 @@ module.exports = {
             { charset: 'utf-8' },
             { name: 'viewport', content: 'width=device-width, initial-scale=1' },
             { hid: 'description', name: 'description', content: pkg.description },
-            { property: 'og:image', content: '/pngs/cgfb.png'}
-
-          
+            { property: 'og:image', content: '/pngs/cgfb.png'}      
         ],
         link: [
             { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
